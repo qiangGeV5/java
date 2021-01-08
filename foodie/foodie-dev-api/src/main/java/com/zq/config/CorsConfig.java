22f4eb8d900ee9e9828d2567d5cd330796e5e9a0
qiangge <qianggev5@126.com>
2021-01-08 19:22:55 +0800

    处理跨域请求问题

---
+++ foodie/foodie-dev-api/src/main/java/com/zq/config/CorsConfig.java
@@ -0,0 +1,40 @@
+package com.zq.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    public CorsConfig(){
+
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        //1添加cors配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:8080"); //可以写星号 ===  *
+
+        //设置是否发送cookie信息
+        config.setAllowCredentials(true);
+
+        //设置允许的请求方式
+        config.addAllowedMethod("*");
+
+        //设置允许的header
+        config.addAllowedHeader("*");
+
+        //2、为url添加映射路径
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",config);
+
+        //3、返回重新定义的corsSouce
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+
+
+    }
+}
