4d6c578f9665a0727a014d594172213bfa92b9fc
qiangge <qianggev5@126.com>
2021-01-08 12:53:08 +0800

    添加result包装类

--- foodie/foodie-dev-api/src/main/java/com/zq/controller/PassportCOntriller.java
+++ foodie/foodie-dev-api/src/main/java/com/zq/controller/PassportCOntriller.java
@@ -1,8 +1,10 @@
 package com.zq.controller;
 
 import com.zq.service.UserService;
+import com.zq.utils.ZQJSONResult;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,22 +18,22 @@ public class PassportCOntriller {
     private UserService userService;
 
     @GetMapping("/usernameIsExist")
-    public int usernameIsExist(@RequestParam String username){
+    public ZQJSONResult usernameIsExist(@RequestParam String username){
 
         //判断用户名是否为空
         if (StringUtils.isBlank(username)){
-            return 500;
+            return ZQJSONResult.errorMsg("用户名不能为空");
         }
 
         //2、查找注册的用户是否存在
         boolean isExist = userService.queryUserNameIsExist(username);
         if (isExist){
-            return 500;
+            return ZQJSONResult.errorMsg("用户名已经存在");
         }
 
 
 
         //请求成功用户名没问题
-        return 200;
+        return ZQJSONResult.ok();
     }
 }
